e76faf9eae5c32b6cf9cce5f3bdaaf2683fe24e0
boris <boris@verhovs.ky>
2021-01-14 11:37:17 -0500

    accept command line arguments and a bunch of todos

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 const puppeteer = require('puppeteer');
 
-const searchString = 'attachEvent';
-const url = 'https://www.wikipedia.org/';
+// TODO: filter out non-text requests and add option to disable that filtering
+// TODO: option to print headers
+// TODO: option to print requests in a transmittable format,
+// ideally "Copy as cURL" in Chrome because that can be converted to Python
+// with https://curl.trillworks.com/
+const args = process.argv.slice(2);  // node index.js
+if (args.length !== 2) {
+    process.exit("usage: node index.js <search string> <url to load>")
+}
+const [searchString, url] = args;
+
+// TODO: add http:// or https://
+new URL(url); // check that second argument can be parsed as a URL
+
+// TODO: handle redirects, for example "wikipedia.org" fails because it
+// redirects to "www.wikipedia.org"
 
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     page.on('response', async response => {
         response.buffer().then(file => {
-            // console.log(response.request().url());
-            // console.log(file);
             if (file.includes(searchString)) {
-                console.log(response.request().url());
+                // TODO: log more here
+                req = response.request();
+                // TODO: better formatting and flag for toggling this, because
+                // the body can be obnoxiously long
+                // if (req.method() === 'POST') {
+                //     console.log(req.method(), req.postData(), req.url());
+                // } else {
+                // TODO: print where in the request the match occured too
+                console.log(req.method(), req.url());
+                // }
             }
-        });
+        }).catch(console.error);
     });
     await page.goto(url);
     await browser.close();
